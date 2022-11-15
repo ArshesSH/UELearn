4f2b2055f3bacd729be3c10d57d6404e6e15563b
ArshesSH <105101004ksh@gmail.com>
2022-11-15 10:34:31 +0900

    Initial commit

---
+++ Source/UELearn/01_Actor/CProperty01.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CProperty01.h"
+
+// Sets default values
+ACProperty01::ACProperty01()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACProperty01::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACProperty01::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UELearn/UELearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UELearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UELEARN_API AUELearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
